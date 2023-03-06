8941e32a48cdbbd65cb84601406652af131e602a
HadertS <source.zero@googlemail.com>
2023-03-06 14:58:12 +0000

    initial Navbar layout commit

---
+++ src/NavComponent/NavComponent.js
@@ -0,0 +1,10 @@
+import "./NavComponent.scss"
+function NavComponent() {
+    return (
+        <nav class="navbar">
+            <h4 class="logo"><span>Food</span>Delivery</h4>
+        </nav>
+    )
+}
+
+export default NavComponent;
