d899b10b9eb5850974c5415071081da166632d01
HadertS <source.zero@googlemail.com>
2023-03-06 15:30:49 +0000

    navComponent JSX and SCSS completed

--- src/NavComponent/NavComponent.js
+++ src/NavComponent/NavComponent.js
@@ -1,8 +1,8 @@
 import "./NavComponent.scss"
 function NavComponent() {
     return (
-        <nav class="navbar">
-            <h4 class="logo"><span>Food</span>Delivery</h4>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <h4 class="navbar-brand my-auto mx-4"><span>Food</span>Delivery</h4>
         </nav>
     )
 }
