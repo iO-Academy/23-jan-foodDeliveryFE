ea674ec41556a6c33e3df140cef1bbc2b29b9dff
alreuben <ayannahreuben@gmail.com>
2023-03-06 14:36:03 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [restaurants, setRestaurants] = useState([])
+    const getRestaurants = () => {
+        fetch('http://localhost:8080/restaurants')
+            .then(data => data.json())
+            .then(restaurants => {
+                console.log(restaurants)
+                setRestaurants(restaurants)
+            })
+    }
+
+    useEffect(() => {
+        getRestaurants()
+    }, [])
+
+
+    return (
+        <>
+            <ul>
+                {restaurants.map(restaurant => <li>{restaurant.name} </li> )}
+            </ul>
+        </>
+    )
+
 }
 
 export default App;
---
+++ src/restaurantDisplay/index.js
@@ -0,0 +1,21 @@
+const restaurantDisplay = ({ restaurant }) => {
+return (
+    <main class="container">
+        <section class="row">
+            <div class="col">{restaurant.name}
+
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+        </section>
+    </main>
+
+)
+
+}
+
+export default restaurantDisplay;
