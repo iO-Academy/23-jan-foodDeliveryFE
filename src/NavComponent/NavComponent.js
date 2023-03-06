343045033d7290847719de55d0e3ab588ab4ae1f
HadertS <source.zero@googlemail.com>
2023-03-06 15:58:14 +0000

    quote format changes for consistancy

--- src/NavComponent/NavComponent.js
+++ src/NavComponent/NavComponent.js
@@ -1,8 +1,8 @@
-import "./NavComponent.scss"
+import './NavComponent.scss'
 function NavComponent() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <h4 class="navbar-brand my-auto mx-4"><span>Food</span>Delivery</h4>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <h4 className='navbar-brand my-auto mx-4'><span>Food</span>Delivery</h4>
         </nav>
     )
 }
