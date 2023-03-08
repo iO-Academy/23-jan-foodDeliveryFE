0b7edc4e43ebd9dd5ee621385ec41eff80a80432
alreuben <ayannahreuben@gmail.com>
2023-03-08 15:52:54 +0000

    col change

--- src/MenuItem/index.js
+++ src/MenuItem/index.js
@@ -2,7 +2,7 @@ import './styles.scss';
 
 const MenuItem = ({menuItemData}) => {
     return (
-        <div className="col col-md-4 col-lg-2">
+        <div className="col-12 col-md-4 col-lg-2">
             <div className="card shadow mb-3 border-dark">
                 <div className="card-body">
                     <h4 className="card-title">{menuItemData.foodName}</h4>
