226c699a8e8a07a6affc2363954c8532b6e3cdbe
HadertS <source.zero@googlemail.com>
2023-03-08 12:18:37 +0000

    code review revisions

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -1,4 +1,4 @@
-import './index.scss'
+import './styles.scss'
 function Nav({restaurantId, setRestaurantId}) {
     const back = () => {
         setRestaurantId(null)
@@ -7,7 +7,7 @@ function Nav({restaurantId, setRestaurantId}) {
     return (
         <nav className='navbar justify-content-center justify-content-lg-between flex-column flex-lg-row navbar-light bg-light px-4 align-items-center'>
             <h4 className='navbar-brand'><span>Food</span>Delivery</h4>
-            {restaurantId && <button className='' onClick={back}>&lt;&lt;Change Restaurant</button>}
+            {restaurantId && <button onClick={back}>&lt;&lt;Change Restaurant</button>}
         </nav>
     )
 }
