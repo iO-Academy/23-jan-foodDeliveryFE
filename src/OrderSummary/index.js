1923f1e84457463bc748623f1ab36f335856e48d
Joperes-ym <joanapereira.ym@gmail.com>
2023-03-09 12:22:19 +0000

    created order summary, not comlete

---
+++ src/OrderSummary/index.js
@@ -0,0 +1,11 @@
+const OrderSummary = () => {
+    return (
+        <div className='col-2 bg-light h-300'>
+            <div>
+              <p>hellooooo</p>
+            </div>
+        </div>
+    )
+}
+
+export default OrderSummary
